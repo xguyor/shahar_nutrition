ab6ebd5a09ef9bd2eb0748adbf2494fc24b0f193
Guy Or <xguyor@gmail.com>
2025-02-14 19:21:54 +0200

    yes baby

--- frontend/src/preview.js
+++ frontend/src/preview.js
@@ -4,7 +4,7 @@ import "./preview.css"; // Import updated CSS
 function PreviewModal({ onEnter }) {
     return (
         <div className="modal-overlay">
-            <div className="modal-content">
+            <div className="modal-content initial">
                 <h1>רוצה להיות בריא יותר?</h1>
                 <p>כנס ותגלה כל מה שאתה צריך כדי לנהל אורח חיים בריא יותר, בזול ובקלות!</p>
                 <button className="enter-btn" onClick={onEnter}>תלחצו לא להתבייש</button>
