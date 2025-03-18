b096fd7f2cd4a7d1a03b322c387c1a70d490fa86
Guy Or <xguyor@gmail.com>
2025-03-18 14:20:08 +0200

    kebab
    Signed-off-by: Shahar Or <shaharor@Shahars-MacBook-Air.local>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import PreviewPage from "./preview";
 import MainPage from "./mainPage";
 import NewsLetterSignup from "./NewsLetterSignup";
 
 function App() {
-    const [showPreview, setShowPreview] = useState(true);
-
-    const handleEnter = () => {
-        setShowPreview(false);
-    };
-
     return (
         <Router>
             <Routes>
-                {/* ✅ Default Route: Shows Preview or Main Page */}
-                <Route
-                    path="/"
-                    element={showPreview ? <PreviewPage onEnter={handleEnter} /> : <MainPage />}
-                />
-
-                {/*landing page*/}
-                <Route path="/mainPage" element={<MainPage />} />
+                {/* ✅ Default Route: Main Page */}
+                <Route path="/" element={<MainPage />} />
 
                 {/* ✅ Direct Route for Newsletter Signup */}
                 <Route path="/newsletter" element={<NewsLetterSignup />} />
